f7bee6add5c5cf8402c6d8007f8c87ec3a7171b7
Calathea-Z <zsykes21@gmail.com>
2022-10-31 21:03:18 -0400

    Complete Act 2, Act 3, and solution.txt

--- act3.sh
+++ act3.sh
@@ -1,5 +1,6 @@
 # 1. Unload the Millenium Falcon in ONE COMMAND!
-# Move the whole crew from the millenium_falcon directory into the death_star directory. HINT: * following a directory will grab all files/folders inside of a directory (directory/*)
+# Move the whole crew from the millenium_falcon directory into the death_star directory. 
+HINT: * following a directory will grab all files/folders inside of a directory (directory/*)
 
 # 2. darth_vader has defeated obi_wan! Delete poor obi_wan.
 
@@ -8,6 +9,7 @@
 
 # 4. Move the millenium_falcon back into the rebellion directory.
 
-# 5. darth_vader leaves the death_star to pursue luke_skywalker! Move him from the death_star into the empire directory!
+# 5. darth_vader leaves the death_star to pursue luke_skywalker! Move him from the death_star 
+into the empire directory!
 
 # 6. Thanks to his practice back home at Beggar’s Canyon, Luke blew up the death_star! Remove it from the galaxy!
